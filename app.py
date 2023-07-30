26e2ba81dcd141983b94bcbe2c4382ea037f6ea6
Daniel Sont <dan.sont@gmail.com>
2023-07-29 22:36:11 -0400

    adding database and mentor import

--- app.py
+++ app.py
@@ -1,57 +1,39 @@
 from flask import Flask
-import psycopg2
-
 app = Flask(__name__)
 
-# connect to postgres
-conn = psycopg2.connect("dbname=mentors user=postgres password=postgres host=db port=5432")
+from db.database import Database
+db = Database()
+
+from db.mentor import create_table, insert, update, delete, get, get_all
+create_table(db)
+
+mentor1 = insert(
+    db=db,
+    first_name="mentor1",
+    last_name="smith",
+    email="smith1@gmail.com",
+    phone="12341234",
+    address="12341234 smith st.",
+    specialization="Designer",
+    experience_years="10",
+    bio="I am a designer",
+)
+
+mentor2 = insert(
+    db=db,
+    first_name="mentor2",
+    last_name="smith",
+    email="smith2@gmail.com",
+    phone="12341235",
+    address="a smith st.",
+    specialization="Receptionist",
+    experience_years="104",
+    bio="I am barely alive",
+)
+
 
 # return json list of mentors
 @app.route("/mentors")
 def mentors():
-    return {
-        "mentors": [
-            {
-                "id": 1,
-                "name": "John Doe",
-                "language": "English",
-                "timezone": "PST",
-                "description": "I'm a professional mentor with 10+ years of experience. I'm a good listener, and I'm very patient. I love helping people and I'm good at explaining things.",
-                "reviews": [
-                    {
-                        "id": 1,
-                        "reviewer": "Jane Doe",
-                        "rating": 4,
-                        "review": "John is a great mentor. He is very patient and explains things very clearly."
-                    },
-                    {
-                        "id": 2,
-                        "reviewer": "Jack Doe",
-                        "rating": 3,
-                        "review": "John is a great mentor. He is very patient and explains things very clearly."
-                    }
-                ]
-            },
-            {
-                "id": 2,
-                "name": "Jane Doe",
-                "language": "English",
-                "timezone": "EST",
-                "description": "I'm a professional mentor with 10+ years of experience. I'm a good listener, and I'm very patient. I love helping people and I'm good at explaining things.",
-                "reviews": [
-                    {
-                        "id": 1,
-                        "reviewer": "John Doe",
-                        "rating": 4,
-                        "review": "Jane is a great mentor. She is very patient and explains things very clearly."
-                    },
-                    {
-                        "id": 2,
-                        "reviewer": "Jack Doe",
-                        "rating": 3,
-                        "review": "Jane is a great mentor. She is very patient and explains things very clearly."
-                    }
-                ]
-            }
-        ]            
-    }
+    mentors = get_all(db=db)
+    return mentors
